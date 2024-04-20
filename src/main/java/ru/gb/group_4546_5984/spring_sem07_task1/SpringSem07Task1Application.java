eb50ead8bf2fd3b34ab5a167c50d6d040540b4e2
alex123 <alex123@dot.com>
2024-04-20 10:57:14 +0500

    start

---
+++ src/main/java/ru/gb/group_4546_5984/spring_sem07_task1/SpringSem07Task1Application.java
@@ -0,0 +1,13 @@
+package ru.gb.group_4546_5984.spring_sem07_task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSem07Task1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSem07Task1Application.class, args);
+	}
+
+}
